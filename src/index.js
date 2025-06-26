0a78438f578d08b259c44c5d645b9d60499b8985
vanisimov4 <144102084+vanisimov4@users.noreply.github.com>
2025-06-26 16:54:31 +0300

    start

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import path from 'node:path';
+import { readContacts } from '../src/utils/readContacts.js';
+
+readContacts();
+
+// const pathToWorkDir = path.join(process.cwd());
+
+// console.log(pathToWorkDir);
--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -1,3 +1,11 @@
+import fs from 'node:fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
 
-export const readContacts = async () => {};
+export const readContacts = async () => {
+  try {
+    const data = await fs.readFile(PATH_DB, 'utf8');
+    console.log('Вміст файлу:', data);
+  } catch (err) {
+    console.error('Помилка читання файлу:', err);
+  }
+};
